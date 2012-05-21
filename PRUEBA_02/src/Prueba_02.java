b06bf4f976e536eee80a3a4364aad02d4a9566c6
jagarcia_1º_Commit <jagarcia@106-07.info2>
2012-05-21 09:26:13 +0200

    21-05-2012 09:24 JOSE ANGEL 3

--- PRUEBA_02/src/Prueba_02.java
+++ PRUEBA_02/src/Prueba_02.java
@@ -13,8 +13,25 @@ public class Prueba_02 {
 		System.out.println("Cambio_04");
 		
 		System.out.println("Cambio_05");
-		System.out.println("Cambio_06");		
+		System.out.println("Cambio_06");
 		
+		System.out.println("Cambio_07");
+		System.out.println("Cambio_08");		
+		
+		System.out.println("Cambio_09");
+		System.out.println("Cambio_10");
+		
+		System.out.println("Cambio_11");
+		System.out.println("Cambio_12");		
+		
+		System.out.println("Cambio_13");
+		System.out.println("Cambio_14");
+		
+		System.out.println("Cambio_15");
+		System.out.println("Cambio_16");		
+		
+		System.out.println("Cambio_17");
+		System.out.println("Cambio_18");
 	}
 
 }
