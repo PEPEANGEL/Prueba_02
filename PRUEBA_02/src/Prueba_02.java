a39519eeb2f9c7f6026197a2fc8d01238fc93017
jagarcia_1º_Commit <jagarcia@106-07.info2>
2012-05-21 09:18:55 +0200

    21_05_2012 09:17 PEPE ANGEL

---
+++ PRUEBA_02/src/Prueba_02.java
@@ -0,0 +1,20 @@
+
+public class Prueba_02 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Cambio_01");
+		System.out.println("Cambio_02");
+		
+		System.out.println("Cambio_03");
+		System.out.println("Cambio_04");
+		
+		System.out.println("Cambio_05");
+		System.out.println("Cambio_06");		
+		
+	}
+
+}
